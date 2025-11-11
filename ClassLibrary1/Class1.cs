de80b506dab81378ac2ea9a14c1d5f0e604d0705
UCLAB814 <UCLAB814@UCLAB814>
2025-11-11 18:57:06 -0500

    Agregar archivos de proyecto.

---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+     public class ArregloNumeros
+    {
+        private int[] numeros;
+        private Random random;
+
+        public ArregloNumeros(int tamaño)
+        {
+            numeros = new int[tamaño];
+            random = new Random();
+        }
+        public void GenerarAleatorios()
+        {
+            for (int i = 0; i < numeros.Length; i++)
+            {
+                numeros[i] = random.Next(1, 11); // entre 1 y 10
+            }
+
+            Console.WriteLine("\nArreglo generado:");
+            Mostrar();
+        }
+
+        // Muestra los elementos del arreglo
+        public void Mostrar()
+        {
+            Console.WriteLine(string.Join(" | ", numeros));
+        }
+
+        //opcion 1: Muestra los elementos elevados a una potencia
+        public void MostrarElevados(int potencia)
+        {
+            Console.WriteLine($"\nElementos elevados a la potencia {potencia}:");
+            foreach (int num in numeros)
+            {
+                double resultado = Math.Pow(num, potencia);
+                Console.Write(resultado + " | ");
+            }
+            Console.WriteLine();
+        }
+
+        // opcion 2: Modifica el valor de una posición específica 
+        public void ModificarValor(int posicion, int nuevoValor)
+        {
+            if (posicion >= 0 && posicion < numeros.Length)
+            {
+                numeros[posicion] = nuevoValor;
+                Console.WriteLine("Valor modificado correctamente.");
+                Mostrar();
+            }
+            else
+            {
+                Console.WriteLine("Posición fuera de rango.");
+            }
+        }
+
+        // opcion 3: Ordena los valores en orden inverso (usando burbuja : usa dos bucles para ordenar de forma inversa)
+        public void MostrarInversoBurbuja()
+        {
+            int[] copia = (int[])numeros.Clone();
+
+            for (int i = 0; i < copia.Length - 1; i++)
+            {
+                for (int j = 0; j < copia.Length - i - 1; j++)
+                {
+                    if (copia[j] < copia[j + 1]) // orden descendente
+                    {
+                        int temp = copia[j];
+                        copia[j] = copia[j + 1];
+                        copia[j + 1] = temp;
+                    }
+                }
+            }
+
+            Console.WriteLine("\nArreglo en orden inverso:");
+            Console.WriteLine(string.Join(" | ", copia));
+        }
+    }
+}
---
+++ S13-EXAMENPRACTICO/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ClassLibrary1;
+
+
+namespace ArregloAleatorio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Ingrese la cantidad de números aleatorios (n) a generar : ");
+            int n = int.Parse(Console.ReadLine());
+
+            // Crear objeto y generar los números aleatorios , llama a la clase  ArregloNumeros y usa (n) dado por el usuario
+            ArregloNumeros arreglo = new ArregloNumeros(n);
+            arreglo.GenerarAleatorios();
+
+            int opcion;
+            do
+            {
+                Console.ForegroundColor = ConsoleColor.Blue;
+                Console.WriteLine("\n----- MENÚ DE OPCIONES -----");
+                Console.WriteLine("[1] Mostrar los elementos elevados a una potencia");
+                Console.WriteLine("[2] Modificar un valor en una posición específica");
+                Console.WriteLine("[3] Mostrar valores en orden inverso");
+                Console.WriteLine("[0] Salir");
+                Console.Write("Seleccione una opción: ");
+                opcion = int.Parse(Console.ReadLine());
+                Console.ResetColor();
+
+                switch (opcion)
+                {
+                    case 1:
+                        Console.ForegroundColor = ConsoleColor.DarkCyan;
+                        Console.Write("Ingrese la potencia: ");
+                        int potencia = int.Parse(Console.ReadLine());
+                        arreglo.MostrarElevados(potencia);
+                        Console.ResetColor();
+                        break;
+
+                    case 2:
+                        Console.ForegroundColor = ConsoleColor.Cyan;
+                        Console.Write("Ingrese la posición a modificar (0 a {0}): ", n - 1);
+                        int pos = int.Parse(Console.ReadLine());
+                        Console.Write("Ingrese el nuevo valor: ");
+                        int nuevoValor = int.Parse(Console.ReadLine());
+                        arreglo.ModificarValor(pos, nuevoValor);
+                        Console.ResetColor();
+                        break;
+
+                    case 3:
+                        Console.ForegroundColor = ConsoleColor.DarkMagenta;
+                        arreglo.MostrarInversoBurbuja();
+                        Console.ResetColor();
+                        break;
+
+                    case 0:
+                        Console.WriteLine("Saliendo del programa...");
+                        break;
+
+                    default:
+                        Console.WriteLine("Opción inválida.");
+                        break;
+                }
+
+            } while (opcion != 0);
+        }
+    }
+
+}
